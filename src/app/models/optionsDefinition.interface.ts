421351c9e5744f7a49b37f965054981eb84c9445
arturgawlik <artur.gawlik@icloud.com>
2019-11-26 23:06:29 +0100

    example json

--- src/app/models/optionsDefinition.interface.ts
+++ src/app/models/optionsDefinition.interface.ts
@@ -2,6 +2,7 @@ export interface OptionsDefinition {
     id: number;
     name: string;
     description: string;
+    comment: string;
     controlType: string;
     starterPrice: number;
     monthlyPrice: number;
